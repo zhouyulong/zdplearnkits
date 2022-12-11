c78a3127ea7ffd6bb7e099009cf08fe466a8b19b
yl-lab-win <zhouyuloong@qq.com>
2022-12-11 21:21:53 +0800

    add fdutils.display_module_layers()

--- fdutils.py
+++ fdutils.py
@@ -3,10 +3,12 @@
 # @Author   : Yu Loong
 # @File     : fdutils.py
 
-from typing import Dict, List, Any, Union, AnyStr, KeysView, Optional
+from typing import Dict, List, Any, Tuple, AnyStr, KeysView, Optional
+import torch
+from torch import nn
 
 
-# c1
+# 1
 class Accumulator():
     """
     collecting metrics in experiment
@@ -42,3 +44,25 @@ class Accumulator():
 
     def get_names(self) -> KeysView:
         return self.accumulator.keys()
+
+
+# 2
+def display_model_layers(module: nn.Module, data_size: Tuple[int, int, int, int]) -> None:
+    tx = torch.normal(0, 1, size=data_size)
+    accumulator = Accumulator(['name', 'input_shape', 'output_shape', 'layer'])
+
+    for layer in module:
+        in_shape = tx.shape
+        tx = layer(tx)
+        out_shape = tx.shape
+        accumulator.add('name', layer.__class__.__name__)
+        accumulator.add('input_shape', in_shape)
+        accumulator.add('output_shape', out_shape)
+        accumulator.add('layer', layer.__str__())
+    max_length_name = max(accumulator['name'], key=len).__len__()
+    max_length_layer = max(accumulator['layer'], key=len).__len__()
+    max_length = len(module)
+    print('\nindex'.center(5), 'name'.center(max_length_name), '\t', 'layer'.center(max_length_layer), '\t', 'input shape'.center(40), '\toutput shape'.center(50), '\n')
+    for index in range(max_length):
+        print(str(index+1).center(5),accumulator['name'][index].ljust(max_length_name), '\t', accumulator['layer'][index].ljust(max_length_layer),
+              '\t', f'input shape\t:{accumulator["input_shape"][index]}'.ljust(40), f'\toutput shape:\t{accumulator["output_shape"][index]}'.ljust(50))
